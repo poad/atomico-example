ee913557cb93cd2cc7b9cc59070e87de4e9d6bf0
Kenji Saito <ken-yo@mbr.nifty.com>
2026-08-04 23:41:22 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo "${CURRENT}"
 cd "${CURRENT}" || exit
 if ! git pull --prune; then
   cd "${CUR}" || exit
-  exit $result
+  exit 1
 fi
 
 cd "${CURRENT}" || exit
@@ -19,7 +19,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-if ! (disable-checkout-persist-credentials && pinact run -u && pnx pnpm@latest self-update && pnpm install -r --no-frozen-lockfile && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm --if-present lint-fix && pnpm build && pnpm install --no-frozen-lockfile); then
+if ! (disable-checkout-persist-credentials && pnx pnpm@latest self-update && pnpm install -r --no-frozen-lockfile && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm --if-present lint-fix && pnpm build && pnpm install --no-frozen-lockfile); then
   cd "${CUR}" || exit
   exit 1
 fi
